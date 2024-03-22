e5293255613ebf199b590fc8d8d877902ab9fa72
Nils Gruson <ngruson@hotmail.com>
2024-03-22 17:01:18 +0100

    Log if custom configuration is being applied

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -1,9 +1,11 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
 var configFolder = builder.Configuration.GetValue<string>("ConfigurationFolder");
+bool customConfig = false;
 if (!string.IsNullOrWhiteSpace(configFolder) &&
   Directory.Exists(configFolder))
 {
+    customConfig = true;
     builder.Configuration.AddKeyPerFile(configFolder, false, true);
 }
 
@@ -14,6 +16,17 @@ builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 builder.AddApplicationServices();
 
 var app = builder.Build();
+
+if (customConfig)
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogInformation("Using custom configuration from {configFolder}", configFolder);
+}
+else
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogInformation("NOT using custom configuration from {configFolder}", configFolder);
+}
 
 app.MapDefaultEndpoints();
 
