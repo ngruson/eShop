133b5c432f08b29eb732e0774efd001294e974c0
Nils Gruson <ngruson@hotmail.com>
2024-04-03 15:48:25 +0200

    Set PathBase again

--- src/Identity.API/IngressMiddleware.cs
+++ src/Identity.API/IngressMiddleware.cs
@@ -12,10 +12,10 @@ public class IngressMiddleware(RequestDelegate next)
             context.RequestServices.GetRequiredService<IServerUrls>().Origin = configuration["Origin"];
         }
 
-        //if (configuration["PathBase"] != null)
-        //{
-        //    context.Request.PathBase = new PathString(configuration["PathBase"]);
-        //}
+        if (configuration["PathBase"] != null)
+        {
+            context.Request.PathBase = new PathString(configuration["PathBase"]);
+        }
 
         await _next(context);
     }
